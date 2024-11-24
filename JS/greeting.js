45348947d3abe195f81d715fd7cf2afe8d4d4254
Andreas Evripidou <83755870+Andreas-Evripidou@users.noreply.github.com>
2024-11-24 22:33:25 +0000

    refactor

--- JS/greeting.js
+++ JS/greeting.js
@@ -1,15 +1,51 @@
-var today = new Date();
-var hour = today.getHours();
-var greeting = "";
+const today = new Date();
+const hour = today.getHours();
 
-if (hour < 12) {
-    greeting = "Good morning!"
+function getGreetingString() {
+    if (hour < 12) {
+       return  "Good morning!";
+    }
+    else if (hour < 18) {
+        return "Good afternoon!";
+    }
+    return "Good evening!";
 }
-else if (hour < 18) {
-    greeting = "Good afternoon!"
-}        
-else {
-    greeting = "Good evening!"
+
+function getCurrentYear() {
+    const year = today.getFullYear();
+    let  element = document.getElementById('currentYear');
+    element.textContent = year;
+    console.log(year);
 }
-element = document.getElementById('greeting');
-element.textContent= greeting;
+
+let element = document.getElementById('greeting');
+element.textContent = getGreetingString();
+
+getCurrentYear();
+
+// function handleScroll(event) {
+//     if ( event.deltaY > 0) {
+//         const currentSection = document.getElementsByClassName('active')[0];
+//         currentSection.classList.remove('active');
+//         let previousSection = currentSection.previousElementSibling;
+//         if (!previousSection) {
+//             previousSection = document.querySelector('.section:last-child');
+//         }
+//         previousSection.classList.add('active');
+//         previousSection.scrollIntoView({behavior: 'smooth'});
+//         return;
+//     }
+    
+//     const currentSection = document.getElementsByClassName('active')[0];
+    
+//     if (currentSection.nextElementSibling === null) {
+//         return;
+//     }
+
+//     currentSection.classList.remove('active');
+//     let nextSection = currentSection.nextElementSibling;
+//     nextSection.classList.add('active');
+//     nextSection.scrollIntoView({behavior: 'smooth'});
+// }
+
+// document.addEventListener('scroll', handleScroll);
